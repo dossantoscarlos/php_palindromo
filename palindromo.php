7ea7aae45ace54956b3fbb8a9a5fb7d41afc7ee8
Carlos Eduardo <anjoazrael10@gmail.com>
2023-09-26 06:49:42 -0300

    Update palindromo.php
    - Adicionando declare .
    - Removendo chaves do ifs

--- palindromo.php
+++ palindromo.php
@@ -1,5 +1,5 @@
 <?php
-
+declare(strict_types=1);
 function verifica_palindromo($texto_processado, $texto_original) {
     $texto_original = explode(' ', trim($texto_original));
     $palavras = explode(' ', trim($texto_processado));
@@ -34,6 +34,5 @@ function execute() {
     }    
 }
 
-if (PHP_SAPI == 'cli') {
+if (PHP_SAPI == 'cli')
     execute();
-}
